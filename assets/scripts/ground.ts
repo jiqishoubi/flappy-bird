ff17761de67b75abaa926ebc3d6b094e9e7fb063
jiqishoubi <lntz2014@gmail.com>
2024-01-03 21:14:24 +0800

    地面滚动

---
+++ assets/scripts/ground.ts
@@ -0,0 +1,19 @@
+import { _decorator, Component, Node } from 'cc'
+const { ccclass, property } = _decorator
+
+@ccclass('ground')
+export class ground extends Component {
+  start() {
+    console.log('🚀 ~ ground start')
+  }
+
+  update(deltaTime: number) {
+    const start = -380
+    const rawWidth = 336
+    let newX = this.node.position.x - 2
+    if (newX < start - rawWidth) {
+      newX = newX + rawWidth
+    }
+    this.node.setPosition(newX, this.node.position.y, this.node.position.z)
+  }
+}
